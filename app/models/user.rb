7731c5fbca24ab28fb39194d292db0180de91ff7
Aimeric Marchau <marchauaimeric@gmail.com>
2022-12-12 08:39:14 +0100

    Created the file seed.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :bookings, dependent: :destroy
+  has_many :requests, dependent: :destroy
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address, presence: true
