7444499926521f6f4363516ce2021b662d1ccb46
Aimeric Marchau <marchauaimeric@gmail.com>
2022-12-19 15:58:28 +0100

    CRUD actions in requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,6 +1,48 @@
 class RequestsController < ApplicationController
+  before_action :set_request, only: %i[show edit update destroy]
 
   def index
     @requests = Request.all
+  end
+
+  def show
+  end
+
+  def new
+    @request = Request.new
+  end
+
+  def create
+    @request = Request.new(request_params)
+    @request.user = current_user
+
+    if @request.save
+      redirect_to request_path(@request)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @request.update(request_params)
+    redirect_to request_path(@request)
+  end
+
+  def destroy
+    @request.destroy
+    redirect_to requests_path, status: :see_other
+  end
+
+  private
+
+  def set_request
+    @request = Request.find(params[:id])
+  end
+
+  def request_params
+    params.require(:request).permit(:title, :description, :price)
   end
 end
